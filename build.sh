0a35a6c5fb5179b27bc39e886941970b95baad0d
Enno Boland <g@s01.de>
2015-02-06 01:35:32 +0100

    build.sh: default build release is the last commit date.

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ UPSTREAM_DIR=/var/www/dl.ffks.de/htdocs/images
 RELEASE=2014.4.x
 
 build_images() {
-	local gluon_branch=
+	local gluon_branch= version=
 	[ "$1" ] && gluon_branch="GLUON_BRANCH=$1"
+	[ "$2" ] && release="GLUON_RELEASE=$2"
 	make update && \
 	make clean && \
-	make $gluon_branch
+	make $gluon_branch $release
 }
 
 sign_images() {
@@ -35,6 +36,8 @@ die() {
 
 cd $(dirname $(which $0))
 branch=`git rev-parse --abbrev-ref HEAD`
+version=$(TZ='Europe/Berlin' date --date "$(git show -s --format=@%ct HEAD)" '+%Y.%m.%d-%H:%M')
+
 if [ "$branch" = master ]; then
 	branch=
 fi
@@ -44,8 +47,10 @@ cd ..
 # check if the release is right:
 [ "$(git rev-parse --abbrev-ref HEAD)" = "$RELEASE" ] || die "We're building on $RELEASE."
 
-build_images $branch || die "Building images failed"
-if [ "$branch" ]; then
+build_images $branch $version || die "Building images failed"
+if [ "$branch" -a "$2"]; then
 	sign_images $branch $2 || die "Signing manifest failed"
 	publish_images $branch || die "Publishing images failed"
+else
+	echo "Warning: Images not signed/published" >&2
 fi
