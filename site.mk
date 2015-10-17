b6c4234e86317a1f8d55feceb505d8b487185135
Philipp Hirsch <itself@hanspolo.net>
2015-10-17 21:04:03 +0200

    Add gateway vpn2 and remove vpn3

--- site.mk
+++ site.mk
@@ -9,6 +9,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-alfred \
 	gluon-announced \
 	gluon-autoupdater \
+        gluon-config-mode-core \
 	gluon-config-mode-hostname \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-mesh-vpn \
@@ -19,9 +20,11 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
 	iptables \
@@ -33,7 +36,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.0
+DEFAULT_GLUON_RELEASE := v2015.1.2
 
 
 ##	GLUON_RELEASE
@@ -48,3 +51,6 @@ GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
+
+# Languages to include
+GLUON_LANGS ?= de en
