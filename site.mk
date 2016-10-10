b3dad6d2854811c2c61525f535b61337881fbc9b
Daniel Rippen <rippendaniel@gmail.com>
2016-10-10 18:40:41 +0200

    Update to v2016.2, change verison to v2016.2-1 for updating

--- site.mk
+++ site.mk
@@ -94,7 +94,7 @@ endif
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := v2016.2
+DEFAULT_GLUON_RELEASE := v2016.2-1
 
 ##	GLUON_RELEASE
 #		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
