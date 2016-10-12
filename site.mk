d0cc6b88eed4e8d863f052a9188d80c6fe555941
Daniel Rippen <rippendaniel@gmail.com>
2016-10-12 21:48:23 +0200

    Change mesh-type to 11s

--- site.mk
+++ site.mk
@@ -113,7 +113,7 @@ GLUON_PRIORITY ?= 0
 GLUON_LANGS ?= de en fr
 
 # Turn on building for ATH10K Devices by specifying mesh type
-GLUON_ATH10K_MESH ?= ibss
+GLUON_ATH10K_MESH ?= 11s
 
 # Print the default release (for external scripts)
 print_default_release:
